79684943a4f18f76e89e9be2a12ad5e057e04cc6
adriannicolai <anlumbad@gmail.com>
2022-07-07 15:24:38 +0800

    updated routes added users

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope "/forms" do
+    get "index"   => "forms#index"
+    post "create" => "forms#create"
+  end
+
+  scope "/users/:api_key" do
+    post "create_user" => "users#create_new_user"
+    post "update_user" => "users#update_user"
+    post "delete_user" => "users#delete_user"
+  end
+
+  root to: 'forms#index'
 end
