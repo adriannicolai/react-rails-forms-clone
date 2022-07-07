6eccfe3330feb21d5546501e891676d7a57f67d5
adriannicolai <anlumbad@gmail.com>
2022-07-07 16:19:05 +0800

    initial commit for create_user

--- server/app/helpers/users_helper.rb
+++ server/app/helpers/users_helper.rb
@@ -1,13 +1,28 @@
 module UsersHelper
     def validate_new_user(new_user_params)
-        validations = {
-            :name =>  { :regex => /^[a-z ,.'-]+$/i, :error => "Special Characters are not allowed" },
-            :email => { :regex => /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/, :error => "Invalid email address" }
-        }
+        response_data = { :status => false, :result => {}, :error => nil }
 
-        new_user_params.each do |key, value|
-            p key
-            p value
+        begin
+            validations = {
+                "name"  => { :regex => /^[a-z ,.'-]+$/i, :error => "Special Characters are not allowed" },
+                "email" => { :regex => /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/, :error => "Invalid email address" }
+            }
+
+            new_user_params.each do |key, value|
+                validation_key = (key === "first_name" || key === "last_name" ? "name" : key)
+
+                next if !validations[validation_key].present?
+
+
+                validation_result = (value =~ validations[validation_key][:regex])
+
+                response_data[:result][validation_key] = validations[validation_key] if !validation_result.nil?
+            end
+        rescue Exception => ex
+            response_data[:error] = ex.message
         end
+
+
+        return response_data
     end
 end
--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,8 +1,9 @@
 include UsersHelper
+include QueryHelper
 
 class User < ApplicationRecord
 
     def self.create_user(new_user_params)
-        
+        validate_new_user_result = validate_new_user(new_user_params)
     end
 end
